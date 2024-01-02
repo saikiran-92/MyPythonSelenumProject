947a4c4023f39502707f0217fd5dffa3d97289a6
Saikiran <149762711+saikiran-92@users.noreply.github.com>
2024-01-02 20:49:04 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(autouse=True, scope='class')
 def setup(request):
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     request.cls.driver = driver
     yield
